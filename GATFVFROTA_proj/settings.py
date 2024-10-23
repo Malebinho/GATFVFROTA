b39b995f21b40fc0cf833c065a97a34e63caa2b9
João Malebo <joaomalebo2017@gmail.com>
2024-10-23 15:54:52 +0100

    file postgres

--- GATFVFROTA_proj/settings.py
+++ GATFVFROTA_proj/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +86,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
