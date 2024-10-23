34eebe67af802aaa320c2cb19a3edadc86012f55
João Malebo <joaomalebo2017@gmail.com>
2024-10-23 16:21:58 +0100

    file postgresfive

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "Building the project"
-python3.11.4 pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Makemigration..."
 
-python3.11.4 manage.py makemigrations --noinput
-python3.11.4 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect static"
-python3.11.4 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 
 
