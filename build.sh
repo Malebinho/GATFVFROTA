f18cd309361345595db2a329171ac5b10ccb7f2c
João Malebo <joaomalebo2017@gmail.com>
2024-10-23 16:43:45 +0100

    file postgreTem

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 
 echo "Building the project"
 python3.9 -m pip install -r requirements.txt
-
-echo "Makemigration..."
-
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Collect static"
 python3.9 manage.py collectstatic --noinput --clear
 
 
